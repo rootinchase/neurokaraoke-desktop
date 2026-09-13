8e073eaab7d6983331927a03a00476ccae7a1c21
rootinchase <rootinchase@gmail.com>
2026-09-12 19:18:38 -0700

    Allow some dead code in the home activity

--- src/activity/home.rs
+++ src/activity/home.rs
@@ -1,10 +1,12 @@
 use eframe::egui::Context;
 
+#[allow(dead_code)]
 #[derive(Debug, Clone)]
 pub struct HomeActivity {
     pub ctx: Context,
 }
 
+#[allow(dead_code)]
 impl HomeActivity {
     pub fn new(ctx: Context) -> Self {
         Self { ctx }
